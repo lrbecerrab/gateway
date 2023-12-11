4996b662431317e068348ab46c53ebb565bd2393
lrbecerrab <lrbecerrab@unal.edu.co>
2023-12-11 04:51:21 -0500

    Update Ethereum and Alchemy API configurations

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,12 @@
 // Contrato Ethereum
-
+const ALCHEMY_API_KEY = "lllJfVK-WoLtGdBK1XmBxzi0a1qfYm2y";
+const signerPrivateKey =
+  "03e12541082027e207b650e4b939576c6b204cebc5ba313e4cfa223b6c775913";
 const ethereumJSON = require("./ethereumMicroGrid.json");
 const ethereumContractAddress = "0xda164db46F0d900F65aC7f3bF2551b897e16A57d";
 const ethereumAbi = ethereumJSON.abi;
 const ethereumProviderUrl =
-
+  "https://eth-sepolia.g.alchemy.com/v2/lllJfVK-WoLtGdBK1XmBxzi0a1qfYm2y";
 
 // Contrato Iota
 const iotaJSON = require("./iotaMicroGrid.json");
