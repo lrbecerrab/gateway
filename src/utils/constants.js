961805911f1989b505834ac1b62c6a0f88e78e27
lrbecerrab <lrbecerrab@unal.edu.co>
2024-01-21 17:13:20 -0500

    Update Ethereum and Iota contract addresses and remove unnecessary API keys

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,21 +1,19 @@
-// Contrato Ethereum
-const ALCHEMY_API_KEY = "lllJfVK-WoLtGdBK1XmBxzi0a1qfYm2y";
-const signerPrivateKey =
-  "03e12541082027e207b650e4b939576c6b204cebc5ba313e4cfa223b6c775913";
 const ethereumJSON = require("./ethereumMicroGrid.json");
-const ethereumContractAddress = "0xda164db46F0d900F65aC7f3bF2551b897e16A57d";
 const ethereumAbi = ethereumJSON.abi;
+const ethereumContractAddress = "0xda164db46F0d900F65aC7f3bF2551b897e16A57d";
 const ethereumProviderUrl =
   "https://eth-sepolia.g.alchemy.com/v2/lllJfVK-WoLtGdBK1XmBxzi0a1qfYm2y";
 
 // Contrato Iota
 const iotaJSON = require("./iotaMicroGrid.json");
-const iotaContractAddress = "0x7B11c1D0f396589a33117C86AA82d66aa1C18888";
 const iotaAbi = iotaJSON.abi;
+const iotaContractAddress = "0x7B11c1D0f396589a33117C86AA82d66aa1C18888";
 const iotaProviderUrl = "https://json-rpc.evm.testnet.shimmer.network";
 
-const apiKeyEtherScan = "U533XNVJCJ5T8MIFEXSRUHQHA3MX82FNKY";
-const apiKeyIotaScan = "108f1e88-a873-470f-a550-91ee20cac5a9";
+// Contrato Ethereum
+const ALCHEMY_API_KEY = "lllJfVK-WoLtGdBK1XmBxzi0a1qfYm2y";
+const signerPrivateKey =
+  "03e12541082027e207b650e4b939576c6b204cebc5ba313e4cfa223b6c775913";
 
 module.exports = {
   ethereumProviderUrl,
@@ -25,6 +23,4 @@ module.exports = {
   iotaContractAddress,
   iotaAbi,
   signerPrivateKey,
-  apiKeyEtherScan,
-  apiKeyIotaScan,
 };
